92518c0d2a4482231d11d2246ff5885dcaf99051
dmitry <bibadima@rambler.ru>
2019-11-26 00:41:06 +0300

    Edited ORF searcher according to new amendments.

--- Bubble_sort&ORF_searcher.py
+++ Bubble_sort&ORF_searcher.py
@@ -2,6 +2,7 @@
 # I hope your depression has come to an end. You're a really good and motivated teacher! Don't take too seriously
 # your problems with administration.
 
+
 def bubble_sort(lst):
     for i in range(len(lst)):
         print(lst)
@@ -18,27 +19,49 @@ x = [5, 3, 2, 5, 6, 1, 6, 7, 4, 3, 9, 10, 4]
 bubble_sort(x)
 
 
-def find_ORFs(sequence):
+def find_ORFs(sequence, reversecomplement=False):
+    """
+    :param sequence: input sequence, where you want to find your ORFS, from 5' to 3'.
+    :param reversecomplement: just something I needed to use recursion. Don't event think about this argument.
+    :return: all the ORFs in the sequence and its reverse compement
+    """
+    sequence = sequence.upper()
     ORFs = []
     for iter in range(3): # we look in al 3 reading frames
-        start, finish = None, None
+        starts, finish = [], None
         seq = sequence[iter:]
         for i in range(0, len(seq), 3):
             codon = seq[i:i+3]
             if codon == "ATG":
-                start = i # open an orf
-            if codon in {"TAG", "TAA", "TGA"} and not start is None: # If there is start and you encounter stop
+                starts.append(i) # open an orf
+            if codon in {"TAG", "TAA", "TGA"} and starts: # If there is start and you encounter stop
                 finish = i+3
-                if i-start >=9: # if there are at least 4 codons
-                    # ATG NNN NNN TAA
-                    # 0           9
-                    # i-start = 9
-                    ORFs.append(seq[start:finish])
-                start = None # Close an orf anyways (even if it's too short)
-    return ORFs
+                for start in starts:
+                    if i-start >=9: # if there are at least 4 codons
+                        # ATG NNN NNN TAA
+                        # 0           9
+                        # i-start = 9
+                        ORFs.append(seq[start:finish])
+                starts = [] # Close an orf anyways (even if it's too short)
+    # Now I add reverse complement ORFs to it.
+    compl = {"A": "T", "T": "A", "C": "G", "G": "C"}
+    additions = []
+    if not reversecomplement:
+        sequence = "".join([compl[letter] for letter in sequence][::-1])
+        additions = find_ORFs(sequence, reversecomplement=True)
+    return ORFs + additions
 
 sq = "ATG" \
      "ACT" \
      "CCT" \
-     "TAATGAAGCTAGTACGTACGTGTACACGATCGGTGCTAGCACTGTACAGTGTCATGACTACACGTGTACGTACTAACATGACATGTGATACACACATAAACTATCGCGGGATCTAACTTA"
+     "TAA" \
+     "TGA" \
+     "ATG" \
+     "TGG" \
+     "ATG" \
+     "GTA" \
+     "CGT" \
+     "GTA" \
+     "TAA" \
+     "GATCGGTGCTAGCACTGTACAGTGTCATGACTACACGTGTACGTACTAACATGACATGTGATACACACATAAACTATCGCGGGATCTAACTTA"
 print(find_ORFs(sq))
