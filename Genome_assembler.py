5859acd7480f7448745ad3584b87162c69771398
dmitry <bibadima@rambler.ru>
2020-04-14 00:48:31 +0300

    De bruujnassemer added

--- Genome_assembler.py
+++ Genome_assembler.py
@@ -1,7 +1,8 @@
-from homework4 import flatten
 import numpy as np
-import re
-from Bio import SeqIO
+import networkx as nx
+from homework4 import flatten
+from collections import Counter
+import os
 
 def match_mismatch(score, penalty, i, j, seq1, seq2):
     """
@@ -125,90 +126,150 @@ def local_align(first, second, match=1, mismatch=-1, gap_open=-5, gap_extend=-1)
     return max(numbers), "\n".join(alignment)
 
 
-def merge_seqs(alignment):
-    seq1, seq2 = alignment.split("\n")
-    query = re.compile(r'^\-+')
-    srch = lambda q, s: len(q.match(s).group(0)) if q.match(s) else 0
-    len1start, len1end = srch(query, seq1), srch(query, seq1[::-1])
-    len2start, len2end = srch(query, seq2), srch(query, seq2[::-1])
-    result = ""
-    if len1start and not len2start:
-        result += seq2[:len1start]
-        start = len1start
-    else:
-        result += seq1[:len2start]
-        start = len2start
-    if len1end and not len2end:
-        result += seq2[start:]
-    else:
-        result += seq1[start:]
-
-    return result
-
-
-def Naive_assembler(file, threshold = 2):
-    records = list(SeqIO.parse(file, format=file.split(".")[-1]))
-    unable_to_merge = False
-    while len(records) != 1 and unable_to_merge != True:
-        print(len(records))
-        i = 0
-        for record in records:
-            i +=1
-            record.id = str(i)
-        alignments = []
-        for i, record1 in enumerate(records):
-            for j, record2 in enumerate(records):
-                if i >= j:
-                    continue
-                #print(record1.id, record2.id)
-                score, alignment = local_align(str(record1.seq), str(record2.seq))
-                alignments.append([score, alignment, record1, record2])
-        alignments.sort(key= lambda element: element[0], reverse=True)
-        records_merged = set()
-        new_reads = []
-        i = 0
-        if alignments[i][0] < threshold:
-            unable_to_merge = True
-        for i in range(len(alignments)):
-            if alignments[i][0] < threshold:
-                break
-            if records_merged.intersection(set([record.id for record in alignments[i][2:]])):
-                continue
-            new_reads.append(SeqIO.SeqRecord(id=str(i), seq=merge_seqs(alignments[i][1])))
-            for element in alignments[i][2:]:
-                records_merged.add(element.id)
-        new_reads += [record for record in records if record.id not in records_merged]
-        records = new_reads
-    return records
-
-
-def shotgun_sequencing(genome, mean_read_length, num_of_reads):
-    reads = []
-    for _ in range(num_of_reads):
-        length = mean_read_length + np.random.randint(-int(mean_read_length*0.1), int(mean_read_length*0.1))
-        start = np.random.randint(0, len(genome)-length)
-        end = start + length
-        reads.append(genome[start:end])
-    return reads
-
 def generate_genome(genome_length):
     genome = ""
     for i in range(genome_length):
         genome += np.random.choice(list("ATGC"))
     return genome
 
-# genome = generate_genome(100)
-# reads = shotgun_sequencing(genome, 10, 100)
-# with open("simulated_reads.fasta", "w") as fl:
-#     i = 1
-#     for read in reads:
-#         fl.write(f">{i}\n{read}\n")
-#         i += 1
-# with open("simulated_genome.fasta", "w") as fl:
-#     fl.write(f">genome\n{genome}")
+def shotgun_sequencing(genome, mean_read_length, num_of_reads):
+    """
+    Errors happen with rate 0.001
+    :param genome: genome to sequence
+    :param mean_read_length: mean read length of your sequenator. You'll get reads in range 0.9mrl - 1.1mrl
+    :param num_of_reads: number of reads you want to get.
+    :return: list of your reads
+    """
+    reads = []
+    for _ in range(num_of_reads):
+        length = mean_read_length + np.random.randint(-int(mean_read_length*0.1), int(mean_read_length*0.1))
+        start = np.random.randint(0, len(genome)-length)
+        end = start + length
+        read = list(genome[start:end])
+        numbers = [0 for _ in range(999)] + [1]
+        for position in range(len(read)):
+            number = np.random.choice(numbers)
+            if number:
+                read[position] = np.random.choice(list(set(list("ATGC")).difference(set([read[position]]))))
+        reads.append("".join(read))
+    return reads
 
-records = Naive_assembler("simulated_reads.fasta", 5)
-print(f'The assembly ended in {len(records)} contigs')
-with open("assembled_genome.fasta", "w") as fl:
-    for record in records:
-        fl.write(f'>{record.id}\n{record.seq}\n')
+def extract_kmers(reads, k):
+    kmers = list()
+    for read in reads:
+        for i in range(len(read)-k+1):
+            kmer = read[i:i+k]
+            kmers.append(kmer)
+    return kmers
+
+def generate_de_bruijn_graph(kmers):
+    graph = nx.MultiDiGraph()
+    for kmer in kmers:
+        graph.add_edge(kmer[:-1], kmer[1:])
+    return graph
+
+def process_de_bruijn_graph(graph):
+    edges = graph.edges
+    edges = [(edge[0], edge[1]) for edge in edges]
+    edges = list(set(edges))
+    froms = [edge[0] for edge in edges]
+    candidates = [node for node in froms if froms.count(node) == 1]
+    falses = set()
+    k = len(candidates[0]) + 1
+    while len(candidates) != 0:
+        candidate = candidates[0]
+        to = list(filter(lambda el: el[0] == candidate, edges))[0][1]
+        tos = [edge[0] for edge in edges]
+        if tos.count(to) != 1:
+            falses.add(candidate)
+        # if len(graph.nodes) > 2:
+        graph = collapse_edge(graph, candidate, to, k)
+        # elif len(graph.nodes) == 2:
+        #     return graph
+        edges = graph.edges
+        edges = [(edge[0], edge[1]) for edge in edges]
+        edges = list(set(edges))
+        print(len(edges))
+        froms = [edge[0] for edge in edges]
+        candidates = list({node for node in froms if froms.count(node) == 1}.difference(falses))
+    return graph
+
+def cut_off_tips(graph, k):
+    edges = list(graph.edges)
+    if edges:
+        candidate = edges[0][0]
+    else:
+        candidate = None
+    false = set()
+    while candidate:
+        print("here")
+        edges_of_interest = list(filter(lambda el: el[0] == candidate, edges))
+        edges_of_interest = [(el[0], el[1]) for el in edges_of_interest]
+        c = Counter(edges_of_interest)
+        pair_of_interest = None
+        for pair in c.keys():
+            if c[pair] == max(c.values()) and list(c.values()).count(max(c.values())) == 1:
+                pair_of_interest = pair
+        if pair_of_interest:
+            graph = collapse_edge(graph, pair_of_interest[0], pair_of_interest[1], k)
+        else:
+            false.add(candidate)
+        edges = list(graph.edges)
+        possible_candidates = {edge[0] for edge in edges}.difference(false)
+        if possible_candidates:
+            candidate = list(possible_candidates)[0]
+        else:
+            candidate = None
+    return graph
+
+def collapse_edge(graph, node1, node2, k):
+    new_graph = nx.MultiDiGraph()
+    new_node = node1[:-(k-2)] + node2
+    current_edges = graph.edges
+    current_nodes = set(graph.nodes)
+    new_neighbours_from = [edge[0] for edge in current_edges if edge[1] == node1]
+    new_neighbours_to = [edge[1] for edge in current_edges if edge[0] == node2]
+    new_graph.add_node(new_node)
+    for node in new_neighbours_from:
+        new_graph.add_edge(node, new_node)
+    for node in new_neighbours_to:
+        new_graph.add_edge(new_node, node)
+    nodes_to_add = current_nodes.difference({node1, node2}).difference(set(new_graph.nodes))
+    for node in nodes_to_add:
+        new_graph.add_node(node)
+    new_edges = [edge for edge in current_edges if not {node1, node2}.intersection(set(edge))]
+    for edge in new_edges:
+        new_graph.add_edge(edge[0], edge[1])
+    return new_graph
+
+def assembler(reads, k=30):
+    kmers = extract_kmers(reads, k)
+    graph = generate_de_bruijn_graph(kmers)
+    graph = process_de_bruijn_graph(graph)
+    graph = cut_off_tips(graph, k=30)
+    nx.nx_pydot.write_dot(graph, 'graph.dot')
+    os.system("dot -Tpng graph.dot > graph.png")
+    os.system("rm graph.dot")
+    contigs = list(graph.nodes)
+    with open("Assembled_genome_de_bruijn.txt", "w") as fl:
+        i = 0
+        for contig in contigs:
+            i += 1
+            fl.write(f">{i}\n{contig}\n")
+    print(f"Congradulations! Your genome is assembled. {len(contigs)} contigs were generated.")
+
+
+### TEST ###
+
+genome = generate_genome(1000)
+with open("simulated_genome.fasta", "w") as fl:
+    fl.write(f">genome\n{genome}")
+
+reads = shotgun_sequencing(genome, 100, 100)
+with open("simulated_reads.fasta", "w") as fl:
+    i = 1
+    for read in reads:
+        fl.write(f">{i}\n{read}\n")
+        i += 1
+
+assembler(reads)
